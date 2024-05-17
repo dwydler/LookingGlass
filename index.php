b478007fc6b8f5f8819616cdc0b8e8382b1c8473
Daniel Wydler <daniel@wydler.eu>
2024-05-17 22:54:42 +0200

    Replaced the name of the author (index.html)

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (file_exists('LookingGlass/Config.php')) {
     <title><?php echo $siteName; ?> - Looking Glass</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="LookingGlass - Open source PHP looking glass">
-    <meta name="author" content="Telephone">
+    <meta name="author" content="Daniel Wydler">
 
     <!-- IE6-8 support of HTML elements -->
     <!--[if lt IE 9]>
