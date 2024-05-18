ad5c853c629d857e1650dd0f05cdf36ba90a9dcf
Daniel Wydler <daniel@wydler.eu>
2024-05-18 19:49:03 +0200

    HTML for W3C Validation, Doctype HTML5 adapted (index.php)

--- index.php
+++ index.php
@@ -22,12 +22,13 @@ if (file_exists('LookingGlass/Config.php')) {
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- General settings -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="LookingGlass - Open source PHP looking glass" />
-    <meta name="author" content="Daniel Wydler" />
+
+	<!-- General settings -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="LookingGlass - Open source PHP looking glass">
+    <meta name="author" content="Daniel Wydler">
 
     <!-- Website title -->
     <title><?php echo $siteName; ?></title>
